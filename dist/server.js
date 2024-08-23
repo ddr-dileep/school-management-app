a13174151d3c87f47a989825ea1ffe2ab216d426
Dileep <ddr.dileepraloti@gmail.com>
2024-08-23 15:02:06 +0530

    initialized project

---
+++ dist/server.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import the 'express' module along with 'Request' and 'Response' types from express
+const express_1 = __importDefault(require("express"));
+// Create an Express application
+const app = (0, express_1.default)();
+// Specify the port number for the server
+const port = 3000;
+// Define a route for the root path ('/')
+app.get('/', (req, res) => {
+    // Send a response to the client
+    res.send('Hello, TypeScript + Node.js + Express!');
+});
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    // Log a message when the server is successfully running
+    console.log(`Server is running on http://localhost:${port}`);
+});
